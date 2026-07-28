d4f13f5916dce7d9254a91972bd5750bffebe5ca
veena <veena@QA>
2026-07-28 16:18:08 -0700

    Commited

--- SeleniumJava/src/test/java/nexusPulse/BaseClass.java
+++ SeleniumJava/src/test/java/nexusPulse/BaseClass.java
@@ -1,12 +1,9 @@
 package nexusPulse;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
 
