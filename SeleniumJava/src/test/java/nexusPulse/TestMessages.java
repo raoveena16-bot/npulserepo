5ca1372803fdefa4558b96a05136f7ec9ef9d3b5
veena <veena@QA>
2026-09-24 11:54:37 -0700

    Testing 'Messages' in Patient Module

---
+++ SeleniumJava/src/test/java/nexusPulse/TestMessages.java
@@ -0,0 +1,40 @@
+package nexusPulse;
+
+import org.nexuspulse.patient.pages.HomePage;
+import org.nexuspulse.patient.pages.LoginPage;
+import org.nexuspulse.patient.pages.MessagePage;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class TestMessages extends BaseClass {
+	
+	LoginPage loginPage;
+	HomePage homePage;
+	MessagePage messagePage;
+	
+	@BeforeClass
+	public void setup()
+	{
+		launchBrowser(prop.getProperty("url"));
+		loginPage = new LoginPage(driver);
+		loginPage.login(prop.getProperty("puname"),prop.getProperty("ppword"));
+		homePage = new HomePage(driver);
+		homePage.navigateToAModule("Messages");
+	}
+	
+	@Test
+	public void testSendMessage() {
+		messagePage = new MessagePage(driver);
+		String randomMessage = AppLibrary.randomString("Msg");
+		messagePage.sendMessage(randomMessage);
+		
+		String displayedMessage = messagePage.checkMessage(randomMessage);
+		
+		Assert.assertEquals(displayedMessage, randomMessage);
+	}
+	
+	
+	
+
+}
